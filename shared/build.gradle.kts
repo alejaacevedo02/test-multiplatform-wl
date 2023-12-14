d0a157b5745da7bd7eccba6ccccfceed3dab6fcc
ACEVEDO URREGO <alejandra.acevedourrego@worldline.com>
2023-12-14 11:52:39 +0100

    build: add dependencies for database

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.compose")
     kotlin("plugin.serialization") version "1.9.21"
+    id("app.cash.sqldelight") version "2.0.0"
 }
 
 kotlin {
@@ -33,6 +34,9 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // For making asynchronous calls
                 implementation("io.ktor:ktor-client-content-negotiation:2.3.4") // Simplify handling of content type based deserialization
                 implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4") // make your dataclasses serializable
+                implementation("app.cash.sqldelight:runtime:2.0.0")
+                implementation("app.cash.sqldelight:coroutines-extensions:2.0.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
             }
         }
         val androidMain by getting {
@@ -40,7 +44,8 @@ kotlin {
                 api("androidx.activity:activity-compose:1.8.1")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.12.0")
-                implementation("io.ktor:ktor-client-android:2.3.4")
+                implementation("io.ktor:ktor-client-okhttp:2.3.4")
+                implementation("app.cash.sqldelight:android-driver:2.0.0")
             }
         }
         val iosX64Main by getting
@@ -54,12 +59,14 @@ kotlin {
 
             dependencies {
                 implementation("io.ktor:ktor-client-darwin:2.3.4")
+                implementation("app.cash.sqldelight:native-driver:2.0.0")
             }
         }
         val desktopMain by getting {
             dependencies {
                 implementation(compose.desktop.common)
                 implementation("io.ktor:ktor-client-apache:2.3.4") // for Desktop
+                implementation("app.cash.sqldelight:sqlite-driver:2.0.0")
             }
         }
     }
